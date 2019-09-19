387f186ccbd74e2fc701fe273938159431bd2677
kevapostol <apostolkevin@gmail.com>
2019-09-19 00:42:08 +0000

    Editted main

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -6,6 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	return (0);
+{	
+	char myStr[] = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(2, myStr, strlen(myStr), 59);
+	return (1);
 }
