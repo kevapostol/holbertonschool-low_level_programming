40d59eaeb746e6712c10f3a3a7057c2219d2f992
kevapostol <apostolkevin@gmail.com>
2019-09-24 18:28:10 +0000

    Edit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,23 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+
+/**
+ * print_alphabet - prints characters from a to z
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch += 1)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
 }
