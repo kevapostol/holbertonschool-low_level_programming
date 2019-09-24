bd778e33e265a8935b6769a1a39022be749edb92
kevapostol <apostolkevin@gmail.com>
2019-09-24 23:24:30 +0000

    Editted file 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints the last digit
+ * @n: the number passed to be tested
+ *
+ * Return: return the n's last digit
+ *
+ */
+
+void jack_bauer(void)
+{
+	int t1;
+	int t2;
+
+	for (t1 = 0; t1 <= 23; t1 += 1)
+	{
+		for (t2 = 0; t2 < 59; t2 += 1)
+		{
+			printf("%02d:%02d\n", t1, t2);
+		}
+	}
+}
