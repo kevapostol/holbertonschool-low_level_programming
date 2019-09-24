a6edcc652dab8479b80250d3c6d750b179dd76b3
kevapostol <apostolkevin@gmail.com>
2019-09-24 21:35:06 +0000

    Editted file 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - Functions and Nested Loops
+ * print_sign - prints the sign of a number
+ * @n: the number passed to be printed
  *
- * Return: Always 0 (Success)
+ * Return: return 1 if positive | return -1 is negative | return 0 is 0
+ *
  */
-
-int main(void)
+int print_sign(int n)
 {
-
-	return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
