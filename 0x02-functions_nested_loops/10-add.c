e927671f7e889274033da4554bb33bc1098e3a0d
kevapostol <apostolkevin@gmail.com>
2019-09-25 03:27:49 +0000

    Editted file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - adds num1 to num2
+ * @num1: integer
+ * @num2: integer
+ *
+ * Return: the sum of num1 and num2
+ *
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+
+	return(sum);
+}
