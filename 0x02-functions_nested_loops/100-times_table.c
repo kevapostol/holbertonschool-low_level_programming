58ef644b0a8ec76f0e9466d8fa9919145ac74ead
kevapostol <apostolkevin@gmail.com>
2019-09-26 03:28:24 +0000

    Editted for adv num 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints the times table, starting with 0 to n
+ * @n: integer
+ *
+ * Return: void
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (!(n > 15) && !(n < 0))
+	{
+		for (i = 0; i <= n; i += 1)
+		{
+			for (j = 0; j <= n; j += 1)
+			{
+				int prod = i * j;
+
+				if (prod < 10 && j != n)
+					printf("%d,   ", prod);
+				else if (prod >= 100 && j != n)
+					printf("%d, ", prod);
+				else if (j == n)
+					printf("%d", prod);
+				else
+					printf("%d,  ", prod);
+			}
+
+			printf("\n");
+		}
+	}
+}
