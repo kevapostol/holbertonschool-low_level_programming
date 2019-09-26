8f6f4fc3864a52e790fccbe5e71442a8f82d744c
kevapostol <apostolkevin@gmail.com>
2019-09-26 19:19:08 +0000

    Editted for problem #9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz
+ *
+ *
+ *
+ * Return: void
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i += 1)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i != 100)
+			printf("%d ", i);
+		else
+			printf("Buzz");
+	}
+
+	printf("\n");
+	return (0);
+}
