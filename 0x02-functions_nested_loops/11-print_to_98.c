04f441c9caabbfecc7d110548b5f0845b925dda1
kevapostol <apostolkevin@gmail.com>
2019-09-26 02:15:46 +0000

    Editted file 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * print_to_98 - prints from n to 98
  * @n: integer
@@ -10,56 +10,20 @@
  */
 void print_to_98(int n)
 {
-	int i;
-	int num0 = 0;
-	int num1 = 0;
-	int num2 = 0;
-	int num;
-
-	if (n > 98)
+	if (n < 98)
 	{
-		for (i = n; i >= 98; i -= 1)
+		for (; n < 98; n += 1)
 		{
-			num0 = (i / 100) % 10;
-			num1 = (i / 10) % 10;
-			num2 = i % 10;
-
-			if (i >= 100)
-				_putchar(num0 + '0');
-
-			if (i >= 10)
-				_putchar(num1 + '0');
-
-			_putchar(num2 + '0');
-			_putchar(' ');
+			printf("%02d, ", n);
 		}
 	}
-	else
+	else if (n > 98)
 	{
-		for (i = n; i <= 98; i += 1)
+		for (; n > 98; n -= 1)
 		{
-			if (i < 0)
-				num = -i;
-			else
-				num = i;
-
-			num1 = (num / 10) % 10;
-			num2 = num % 10;
-
-			if (i < 0)
-				_putchar('-');
-
-			if (i >= -9 && i <= 9)
-				_putchar(num2 + '0');
-			else
-			{
-				_putchar(num1 + '0');
-				_putchar(num2 + '0');
-			}
-
-			_putchar(' ');
+			printf("%02d, ", n);
 		}
 	}
 
-	_putchar('\n');
+	printf("98\n");
 }
