f1bbdec72cbc599c14c842ab42225a278802ec81
kevapostol <apostolkevin@gmail.com>
2019-09-26 16:33:26 +0000

    Editted for problem #2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: number a
+ * @b: number b
+ *
+ * Return: return product
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
