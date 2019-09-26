0b93ea472b40523fc9a22b7a4cf0b55e87246836
kevapostol <apostolkevin@gmail.com>
2019-09-26 20:07:07 +0000

    Editted for problem #10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * print_triangle - prints a triangle followed by a newline
@@ -16,20 +15,20 @@ void print_triangle(int size)
 	int space_num = size - 1;
 
 	if (size == 0 || size < 0)
-		printf("\n");
+		_putchar('\n');
 	else
 	{
 		for (i = 0; i < size; i += 1)
 		{
 			for (j = 0; j < space_num; j += 1)
-				printf(" ");
+				_putchar(' ');
 			space_num -= 1;
 
 			for (j = 0; j <= char_num; j += 1)
-				printf("#");
+				_putchar('#');
 			char_num += 1;
 
-			printf("\n");
+			_putchar('\n');
 		}
 	}
 }
