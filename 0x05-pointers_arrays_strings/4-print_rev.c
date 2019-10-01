3f21c47b8d7de9700476b949dfab2cb5bf548920
kevapostol <apostolkevin@gmail.com>
2019-10-01 17:18:19 +0000

    Editted file 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,38 +1,38 @@
 #include "holberton.h"
 /**
- * swap_int - swaps the values of two integers
- * @*a: pointer
- * @*b: pointer
+ * _strlen - gets the lenght
+ * @s: pointer
  *
- * Return: returns nothing
+ *
+ * Return: returns int
  *
  */
 int _strlen(char *s)
 {
 	int n;
-	
+
 	for (n = 0; *s != '\0'; s++)
 		n++;
 
-	return n;
+	return (n);
 }
 
 /**
- * swap_int - swaps the values of two integers
- * @*a: pointer
- * @*b: pointer
+ * print_rev -  prints a string, in reverse, followed by a new line
+ * @s: pointer
+ *
  *
  * Return: returns nothing
  *
  */
 void print_rev(char *s)
-{	
+{
 	int len = _strlen(s) - 1;
 
 	for (; len >= 0; len--)
 	{
 		_putchar(s[len]);
-	}	
-	
+	}
+
 	_putchar('\n');
 }
