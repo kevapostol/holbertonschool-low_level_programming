8bd159b15231039a844d8a6f22686c3e7eadfcd0
kevapostol <apostolkevin@gmail.com>
2019-10-03 22:48:48 +0000

    Editted file 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,30 @@
 #include "holberton.h"
 
 /**
- * reverse_array - reverses the content of an array of integers
- * @a: pointer, array
- * @n: integer, length of array
+ * string_toupper - reverses the content of an array of integers
+ * @c: pointer, array
+ *
  *
  * Return: no return
  *
  */
-char *string_toupper(char *)
+char *cap_string(char *c)
 {
-	
+	int ctr = 0;
+
+	while (c[ctr])
+	{
+		if (c[ctr] == ' ' || c[ctr] == '\t' || c[ctr] == '\n' || c[ctr] == ',' ||
+			c[ctr] == ';' || c[ctr] == '.' || c[ctr] == '!' || c[ctr] == '?' ||
+			c[ctr] == '"' || c[ctr] == '(' || c[ctr] == ')' || c[ctr] == '{' || 
+			c[ctr] == '}')
+		{
+			if (c[ctr + 1] >= 'a' && c[ctr + 1] <= 'z')
+			{
+				c[ctr + 1] -= 32;
+			}
+		}
+		ctr++;
+	}
+	return (c);
 }
