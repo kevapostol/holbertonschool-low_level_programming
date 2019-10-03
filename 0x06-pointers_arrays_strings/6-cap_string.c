60aca6781f1a5bb002a822ec159470a0587312a5
kevapostol <apostolkevin@gmail.com>
2019-10-03 22:14:19 +0000

    Editted file 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: pointer, array
+ * @n: integer, length of array
+ *
+ * Return: no return
+ *
+ */
+char *string_toupper(char *)
+{
+	
+}
