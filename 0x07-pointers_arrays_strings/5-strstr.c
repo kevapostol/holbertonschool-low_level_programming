8a084eec80a5785671e35810c900a53e4456484e
kevapostol <apostolkevin@gmail.com>
2019-10-07 22:40:16 +0000

    Created and Editted for problem 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * comp - compares 2 strings
+ * @x: string 1
+ * @y: string 2
+ *
+ * Return: true or false, integer
+ *
+ */
+int comp(char *x, char *y)
+{
+	while (*x && *y)
+	{
+		if (*x != *y)
+			return (0);
+		x++;
+		y++;
+	}
+
+	return (*y == '\0');
+}
+
+/**
+ * _strstr - unction that locates a substring
+ * @haystack: pointer, an array
+ * @needle: pointer, an array
+ *
+ * Return: string, char pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if (*haystack == *needle && comp(haystack, needle))
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
