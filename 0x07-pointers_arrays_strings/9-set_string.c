ff82ea64299ec1a87e353bbe66d9e47e45da47f6
kevapostol <apostolkevin@gmail.com>
2019-10-08 05:07:40 +0000

    Created and Editted file for problem 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strspn - function that locates a character in a string
+ * @s: pointer, an array
+ * @accept: pointer, an array
+ *
+ * Return: count, integer
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
