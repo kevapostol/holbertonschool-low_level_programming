43c2c8c273c00a88621c92ea0a1d4f8025129fc1
kevapostol <apostolkevin@gmail.com>
2019-10-12 20:41:29 +0000

    Created a solution for problem 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: void returns nothing
+ *
+ */
+int main(int argc , char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
