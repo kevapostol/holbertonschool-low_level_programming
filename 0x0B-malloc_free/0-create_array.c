ce42be67ceaa475fe3fc14892f3132061e76ef0c
kevapostol <apostolkevin@gmail.com>
2019-10-15 19:50:52 +0000

    Created .c files and header file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,17 @@ char *create_array(unsigned int size, char c)
 {
 	if (size == 0)
 		return (NULL);
-
-	char *arr = (char *) malloc(size * sizeof(char));
-
-	unsigned int i = 0;
-
-	while (i < size)
+	else
 	{
-		arr[i] = c;
-		i++;
-	}
+		char *arr = (char *) malloc(size * sizeof(char));
+		unsigned int i = 0;
 
-	return (arr);
+		while (i < size)
+		{
+			arr[i] = c;
+			i++;
+		}
+
+		return (arr);
+	}
 }
