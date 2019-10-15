35fd82d7433f2460cc6c320f338c7d7ee9be5a4a
kevapostol <apostolkevin@gmail.com>
2019-10-15 21:48:23 +0000

    Added edge case if NULL is passed, treat it as an empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,12 @@ char *str_concat(char *s1, char *s2)
 	int ctrS1 = 0, ctrS2 = 0, i = 0, j = 0;
 	char *st;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[ctrS1] != '\0')
 		ctrS1++;
 
