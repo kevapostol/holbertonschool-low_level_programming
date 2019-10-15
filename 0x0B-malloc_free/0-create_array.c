7b050318d13ba5650a733861be36eedd8de50928
kevapostol <apostolkevin@gmail.com>
2019-10-15 20:01:57 +0000

    Modified source code for handling returns

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,9 @@ char *create_array(unsigned int size, char c)
 		char *arr = (char *) malloc(size * sizeof(char));
 		unsigned int i = 0;
 
+		if (arr == NULL)
+			return (NULL);
+
 		while (i < size)
 		{
 			arr[i] = c;
