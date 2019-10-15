82cf7a5eb20754e92477bd3c39553deeca070a7c
kevapostol <apostolkevin@gmail.com>
2019-10-15 21:40:39 +0000

    Created and solved problem 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: char pointer pointed to 1st idx of the array
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int ctrS1 = 0, ctrS2 = 0, i = 0, j = 0;
+	char *st;
+
+	while (s1[ctrS1] != '\0')
+		ctrS1++;
+
+	while (s2[ctrS2] != '\0')
+		ctrS2++;
+
+	st = malloc((ctrS1 + ctrS2 + 1) * sizeof(char));
+
+
+	while (s1[i] != '\0')
+	{
+		st[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		st[i] = s2[j];
+		i++;
+		j++;
+	}
+	st[i] = '\0';
+	return (st);
+}
