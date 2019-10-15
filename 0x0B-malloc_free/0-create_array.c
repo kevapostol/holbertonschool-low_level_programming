619c88db04e50f9080692cc5619f9fcd0e1a4874
kevapostol <apostolkevin@gmail.com>
2019-10-15 20:28:14 +0000

    Removed typecast

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		char *arr = (char *) malloc(size * sizeof(char));
+		char *arr = malloc(size * sizeof(char));
 		unsigned int i = 0;
 
 		if (arr == NULL)
