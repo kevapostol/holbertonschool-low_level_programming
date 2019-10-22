3a3bbf7d50a14bfb622e83b43a0672d034925b0d
kevapostol <apostolkevin@gmail.com>
2019-10-22 02:55:49 +0000

    Modified header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -14,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 		return;
 
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
