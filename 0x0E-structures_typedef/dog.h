c056c5f115fce54376b4a4d8fb197e86c5efece6
kevapostol <apostolkevin@gmail.com>
2019-10-22 02:23:04 +0000

    Created a header file that has a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Long Desc
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
