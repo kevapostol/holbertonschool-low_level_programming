3663811d17f2aec26e8a588b0e4f6cf1888ef2bf
kevapostol <apostolkevin@gmail.com>
2019-10-23 18:00:14 +0000

    Solution for the function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: character pointer of a name
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
