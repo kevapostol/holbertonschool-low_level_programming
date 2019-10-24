fd3747a3cbe0ba4885c71c7842e672d04a5bc2de
kevapostol <apostolkevin@gmail.com>
2019-10-24 05:40:03 +0000

    Modified source code to handle error 99

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,10 +14,16 @@ int main(int argc, char *argv[])
 	int num2 = atoi(argv[3]);
 	int result;
 
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
+	}
+	if ((get_op_func)(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
 	{
