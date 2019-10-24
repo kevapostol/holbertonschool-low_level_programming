412979c0f374b1827489bd11a5ea0369126f9a74
kevapostol <apostolkevin@gmail.com>
2019-10-24 03:48:02 +0000

    Solves calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	int result;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ * @a: number 1
+ * @b: number 2
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts
+ * @a: number 1
+ * @b: number 2
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies
+ * @a: number 1
+ * @b: number 2
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides
+ * @a: number 1
+ * @b: number 2
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: number 1
+ * @b: number 2
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
