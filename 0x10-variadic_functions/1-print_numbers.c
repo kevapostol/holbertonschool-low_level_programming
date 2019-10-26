5858c1b3893316f09e314c89acf0850ca3b49963
FIRST_NAME LAST_NAME <apostolkevin@gmail.com>
2019-10-26 22:43:30 +0000

    Editted conditionals

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,14 +12,13 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list var;
-	int i = 0, temp;
+	int i;
 
 	va_start(var, n);
 
 	for (i = 0; i < (int) (n - 1); i++)
 	{
-		temp = va_arg(var, int);
-		printf("%d%s", temp, separator);
+		printf("%d%s", va_arg(var, int), separator);
 	}
 
 	printf("%d\n", va_arg(var, int));
