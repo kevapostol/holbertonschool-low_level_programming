a294b60a655ae4a3fc535448a5cd003fecdf657f
kevapostol <apostolkevin@gmail.com>
2019-10-31 22:07:46 +0000

    Solves a function that adds a new node at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * _strCount - returns the number of elements of a string
+ * @str: char pointer to a stirng
+ *
+ * Return: int, len of string
+ */
+int _strCount(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: head pointer
+ * @str: string pointer
+ * Return: returns a pointer that points to new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	new->len = _strCount(str);
+	new->str = strdup(str);
+
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
