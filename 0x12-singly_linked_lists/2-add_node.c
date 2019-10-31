b59520ce7ac11ffa75b2e1920a3cc440d5ce1ee3
kevapostol <apostolkevin@gmail.com>
2019-10-31 22:50:05 +0000

    Handles memory leaks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,11 +27,26 @@ int _strCount(const char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new;
 
+	if (!head || !*head || !str)
+	{
+		return (NULL);
+	}
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->len = _strCount(str);
 	new->str = strdup(str);
-
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->next = *head;
 	*head = new;
 
