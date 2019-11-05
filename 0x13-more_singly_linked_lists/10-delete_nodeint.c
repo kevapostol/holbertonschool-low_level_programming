814804a7acb07b67f13380df9e4503d4c804c5de
kevapostol <apostolkevin@gmail.com>
2019-11-05 23:54:22 +0000

    Deletes a node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prev = NULL;
 	unsigned int idx_ctr = 0;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	temp = *head;
@@ -32,6 +32,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		prev = temp;
 		temp = temp->next;
 		idx_ctr += 1;
+	}
+
+	if (index != idx_ctr)
+	{
+		return (-1);
 	}
 
 	if (prev == NULL)
