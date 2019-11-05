4fbda0f7cda4a2aca13efcb47b41cfd0c85c4170
kevapostol <apostolkevin@gmail.com>
2019-11-05 23:09:03 +0000

    Added edge case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*check for malloc*/
 	if (new == NULL)
 		return (NULL);
-
+	/*handles head is null*/
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		return (new);
+	}
 	/*edge case for idx = 0*/
 	if (idx == 0)
 	{
