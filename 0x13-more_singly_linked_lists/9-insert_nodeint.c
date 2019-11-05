c68c8b40e9a561fab8012e65d8a537569b39374e
kevapostol <apostolkevin@gmail.com>
2019-11-05 23:35:24 +0000

    Edit conditional

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *prev, *new;
 
-	if (head == NULL || idx < 0)
+	if (head == NULL)
 		return (NULL);
 	if (*head == NULL && idx != 0)
 		return (NULL);
