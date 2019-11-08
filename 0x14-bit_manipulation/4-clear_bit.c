2e413ee43a62a3ccc0b95450bd004b281e365289
kevapostol <apostolkevin@gmail.com>
2019-11-08 00:19:52 +0000

    Sovles a function that sets the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: number
+ * @index: index
+ * Return: returns 1 if succes else -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	unsigned int i = 0;
+
+	if (*n == 0 && index > 0)
+	{
+		*n = 0;
+		return (1);
+	}
+
+	while (mask > 0)
+	{
+		if (i == index)
+		{
+			*n = (mask ^ *n);
+			return (1);
+		}
+
+		i += 1;
+		mask = mask << 1;
+	}
+
+	return (-1);
+}
