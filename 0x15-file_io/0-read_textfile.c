135eaff77bf6cea5e8ed0d0b78a7c667fb79a460
kevapostol <apostolkevin@gmail.com>
2019-11-12 23:06:58 +0000

    Fix bug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,14 +24,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
+	{
+		free(buf);
 		return (0);
-
+	}
 	count = read(fd, buf, letters);
-	if (count == -1)
+	if (count < 0)
+	{
+		free(buf);
 		return (0);
+	}
 	if (write(1, buf, letters) == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	close(fd);
 	free(buf);
 
