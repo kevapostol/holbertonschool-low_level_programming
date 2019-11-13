f6ee69f24185a192d8ebcec4fa5692c551282eb2
kevapostol <apostolkevin@gmail.com>
2019-11-13 01:01:22 +0000

    Added edge case to handle NULL text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, strLen;
+	int fd, strLen, w_stat;
 	char *buff;
 
 	if (filename == NULL)
@@ -30,11 +30,15 @@ int create_file(const char *filename, char *text_content)
 	if (buff == NULL)
 		return (-1);
 
-	if (write(fd, text_content, strLen) < 0)
+	if (text_content == NULL)	
+		w_stat = write(fd, text_content, strLen);
+	else
+		w_stat = write(fd, text_content, strLen);
+	if(w_stat < 0)
 	{
 		free(buff);
 		return (-1);
 	}
-
+	close(fd);
 	return (1);
 }
