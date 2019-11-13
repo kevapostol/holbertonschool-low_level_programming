25265f5e27923f4d988a471bb238c6361ce846c6
kevapostol <apostolkevin@gmail.com>
2019-11-13 01:21:36 +0000

    edit edge case

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
@@ -25,14 +28,7 @@ int create_file(const char *filename, char *text_content)
 	for (strLen = 0; text_content[strLen]; strLen++)
 		;
 
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
-	else
-		write(fd, text_content, strLen);
-
+	write(fd, text_content, strLen);
 	close(fd);
 	return (1);
 }
