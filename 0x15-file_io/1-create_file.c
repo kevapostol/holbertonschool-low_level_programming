4bcc3b438d4f82238475a3d5ea966c569f9f0f04
kevapostol <apostolkevin@gmail.com>
2019-11-13 01:14:28 +0000

    Edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,32 +14,28 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, strLen, w_stat;
-	char *buff;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 00600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
 	for (strLen = 0; text_content[strLen]; strLen++)
 		;
 
-	buff = malloc(sizeof(char) * strLen + 1);
-	if (buff == NULL)
-		return (-1);
-
 	if (text_content == NULL)
-		w_stat = write(fd, "", strLen);
+	{
+		close(fd);
+		return (1);
+	}
 	else
 		w_stat = write(fd, text_content, strLen);
 	if (w_stat < 0)
 	{
-		free(buff);
 		return (-1);
 	}
 	close(fd);
-	free(buff);
 	return (1);
 }
