6e5bcdd499fa1bc9fd63d6d1ddd519dcb645dc99
kevapostol <apostolkevin@gmail.com>
2019-12-15 01:10:40 +0000

    Added an edge case where index inserts a node at the end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,12 +22,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new->prev = NULL;
 
 	if (*h == NULL || idx == 0)
-	{
 		return (add_dnodeint(h, n));
-	}
 
 	for (i = 0; temp != NULL; i++, temp = temp->next)
 	{
@@ -40,6 +37,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (new);
 		}
 	}
+
+	if (i == idx)
+		return (add_dnodeint_end(h, n));
 
 	free(new);
 	return (NULL);
