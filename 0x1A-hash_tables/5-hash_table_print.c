20f08a492407d6fa1618f7ee50ffc2a384936e0f
kevapostol <apostolkevin@gmail.com>
2020-01-19 03:01:32 +0000

    Prints a HT

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,6 @@ void hash_table_print(const hash_table_t *ht)
 	int idx = 0;
 	hash_node_t *head;
 	int flag = 0;
-	int flag_first = 1;
 
 	if (ht == NULL)
 		return;
@@ -23,19 +22,14 @@ void hash_table_print(const hash_table_t *ht)
 		head = ht->array[idx];
 
 		if (ht->array[idx] != NULL)
-		{
-			if (head == NULL)
-				flag = 0;
-			else if (head != NULL)
-				flag = 1;
-
-			if (flag == 1 && flag_first == 0)
+		{	
+			if (flag == 1)
 				printf(", ");
 
 			/* prints key and value*/
 			printf("'%s': '%s'", head->key, head->value);
 
-			flag_first = 0;
+			flag = 1;
 			head = head->next;
 		}
 		idx++;
