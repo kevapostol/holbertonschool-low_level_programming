01d463834d711212eb70d92dfbbc87d4fc6dd0dc
kevapostol <apostolkevin@gmail.com>
2020-01-19 00:50:21 +0000

    Added function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,4 +42,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int attach_node_to_ht(hash_table_t *ht, hash_node_t *node);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
