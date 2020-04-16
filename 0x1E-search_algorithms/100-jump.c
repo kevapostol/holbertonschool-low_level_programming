816acb6c137c6c190a7606ee52ef036720bf9d4c
kevapostol <apostolkevin@gmail.com>
2020-04-16 02:00:00 +0000

    Handles null values

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@ int jump_search(int *array, size_t size, int value)
 	int prev = 0, x;
 	int step = (int)floor(sqrt(size));
 
-	if (array == null || size == 0)
+	if (array == NULL || size == 0)
 		return (-1);
 	printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
 	while (array[step] <= value && step < (int)size)
